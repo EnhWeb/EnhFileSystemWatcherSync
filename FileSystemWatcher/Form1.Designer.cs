69cff62a6b7785b8b0006a3d1c30a5ec74fed3cf
263646469@qq.com <263646469@qq.com>
2016-11-15 13:00:04 +0800

    第一次提交
    Signed-off-by: 263646469@qq.com <263646469@qq.com>

---
+++ FileSystemWatcher/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace FileSystemWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.myWatcher = new System.IO.FileSystemWatcher();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.myWatcher)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Location = new System.Drawing.Point(82, 242);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(75, 23);
+            this.btnBegin.TabIndex = 0;
+            this.btnBegin.Text = "开始监视";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // myWatcher
+            // 
+            this.myWatcher.EnableRaisingEvents = true;
+            this.myWatcher.SynchronizingObject = this;
+            this.myWatcher.Renamed += new System.IO.RenamedEventHandler(this.myWatcher_Renamed);
+            this.myWatcher.Changed += new System.IO.FileSystemEventHandler(this.myWatcher_Changed);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(222, 253);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(113, 12);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "尚未开始监视文件夹";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FTP地址:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "FTP用户名:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(149, 77);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(186, 21);
+            this.txtAddress.TabIndex = 4;
+            this.txtAddress.Text = "ftp://203.171.236.98:21//";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "FTP密码:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(149, 184);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(186, 21);
+            this.textBox3.TabIndex = 7;
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(149, 24);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(186, 21);
+            this.txtFolder.TabIndex = 8;
+            this.txtFolder.Text = "E:\\YooKee\\WebUI\\";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "监视目录:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 305);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnBegin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "镜像服务器文件实时同步程序";
+            ((System.ComponentModel.ISupportInitialize)(this.myWatcher)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBegin;
+        private System.IO.FileSystemWatcher myWatcher;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFolder;
+
+    }
+}
+
---
+++ FileSystemWatcher/Form1.cs
@@ -0,0 +1,293 @@
+/*------------------------------------------------
+// YooKee
+// Copyright (C) 2006-2008 
+//
+// 文件名:Form1.cs
+// 文件功能描述：镜像服务器文件实时同步程序
+//
+// 创建人：Sunshine
+// 创建日期: 2007.12.11
+// 
+// 修改标识:2007.12.13
+// 修改描述:增加通过XML操作功能
+//
+ ------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace FileSystemWatcher
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 记录文件更新总数
+        /// </summary>
+        private int sum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 点击开始监视按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            //要监视的目录
+            string sFolder = txtFolder.Text.Trim();
+            myWatcher.EnableRaisingEvents = true;
+            myWatcher.Path = sFolder;
+            myWatcher.IncludeSubdirectories = true;
+            myWatcher.NotifyFilter = NotifyFilters.Size;
+            lblMessage.Text = "正在监视中...";
+
+        }
+
+        /// <summary>
+        /// 当文件改变时进行操作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void myWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Deleted)
+            {
+                return;
+            }
+
+            if (e.Name.Length > 12)
+            {
+                string flvFile = e.Name.Substring(0, 11);
+
+                if (flvFile == "UploadVideo")
+                {
+                    return;
+                }
+            }
+
+            System.IO.FileInfo fileinfo = new FileInfo(e.FullPath);
+
+            int i = 1;
+        Finish:
+            try
+            {
+                if (i < 10)
+                {
+                    fileinfo.OpenRead();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch
+            {
+                System.Threading.Thread.Sleep(6000);
+                i = i + 1;
+                goto Finish;
+            }
+
+            //将变动信息写入XML.
+            WriterFileXML(e.Name, e.FullPath);
+
+            //从XML中读取文件信息并上传.
+            ReaderFileXML();
+
+            lblMessage.Text = sum.ToString() + " 文件已成功分发!";
+        }
+
+
+        /// <summary>
+        /// 将改动过的文件记录到XML中.
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <param name="fullPath">完整路径</param>
+        private void WriterFileXML(string fileName, string fullPath)
+        {
+
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                doc.Load("E:\\WatcherFiles.xml");
+            }
+            catch(Exception e)
+            {
+                WriteLog(e.ToString());
+            }
+
+            XmlNode root = doc.SelectSingleNode("WatcherFiles");
+           
+            XmlElement fileinfo = doc.CreateElement("FileInfo");
+
+            sum += 1;
+
+            XmlElement id = doc.CreateElement("ID");
+            id.InnerText = sum.ToString();
+            fileinfo.AppendChild(id);
+
+            //文件名
+            XmlElement fn = doc.CreateElement("FileName");
+            fn.InnerText = fileName;
+            fileinfo.AppendChild(fn);
+
+            //完整路径
+            XmlElement fp = doc.CreateElement("FullPath");
+            fp.InnerText = fullPath;
+            fileinfo.AppendChild(fp);
+
+            //保存时间
+            XmlElement st = doc.CreateElement("SaveTime");
+            st.InnerText = DateTime.Now.ToString();
+            fileinfo.AppendChild(st);
+
+            //0是否已ftp到网通0否,1是，2出错
+            XmlElement isWT = doc.CreateElement("IsFTPWT");
+            isWT.InnerText = "0";
+            fileinfo.AppendChild(isWT);
+
+            //FTP到网通开始时间
+            XmlElement fsWT = doc.CreateElement("FtpWTStartTime");
+            fsWT.InnerText = "0";
+            fileinfo.AppendChild(fsWT);
+
+            //FTP到网通完成时间
+            XmlElement feWT = doc.CreateElement("FtpWTEndTime");
+            feWT.InnerText = "0";
+            fileinfo.AppendChild(feWT);
+
+
+            //0是否已ftp到电信0否,1是，2出错
+            XmlElement isDX = doc.CreateElement("IsFTPDX");
+            isDX.InnerText = "0";
+            fileinfo.AppendChild(isDX);
+
+            //FTP到电信开始时间
+            XmlElement fsDX = doc.CreateElement("FtpDXStartTime");
+            fsDX.InnerText = "0";
+            fileinfo.AppendChild(fsDX);
+
+            //FTP到电信完成时间
+            XmlElement feDX = doc.CreateElement("FtpDXEndTime");
+            feDX.InnerText = "0";
+            fileinfo.AppendChild(feDX);
+
+            root.AppendChild(fileinfo);
+            doc.Save("E:\\WatcherFiles.xml");
+
+        }
+
+        /// <summary>
+        /// 使用FTP上传文件
+        /// </summary>
+        private bool UseFTP(string fileName,string fullName)
+        {
+            string smallName = fileName.Replace(@"\", @"/");
+
+            WebClient client = new WebClient();
+            
+            //FTP用户名，密码.
+            client.Credentials = new NetworkCredential("YooYoCN", "200712101048YooYoCN");
+
+            string strAddress = txtAddress.Text.Trim();
+            Uri url = new Uri(strAddress +smallName);
+
+            try
+            {
+                //FTP执行
+                client.UploadFile(url,fullName);
+            }
+            catch (Exception e)
+            {
+                WriteLog(e.ToString());
+
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 读取XML
+        /// </summary>
+        private void ReaderFileXML()
+        {
+            XmlDocument xd = new XmlDocument();
+            xd.Load("E:\\WatcherFiles.xml");
+
+            XmlNodeList xnl = xd.SelectSingleNode("WatcherFiles").ChildNodes;
+
+            foreach (XmlNode xn in xnl)
+            {
+                //取得FileInfo节点
+                XmlElement fie = (XmlElement)xn;
+
+                if (fie.SelectSingleNode("IsFTPWT").InnerText == "0")
+                {
+                    string isWT = fie.SelectSingleNode("IsFTPWT").InnerText;
+
+                    string fileName = fie.SelectSingleNode("FileName").InnerText;
+
+                    string fullPath = fie.SelectSingleNode("FullPath").InnerText;
+
+                    //判断文件是否存在.
+                    if (System.IO.File.Exists(fullPath) == true)
+                    {
+                        string startWTime = DateTime.Now.ToString();
+
+                        //上传到网通服务器
+                        if (UseFTP(fileName, fullPath))
+                        {
+                            string endWTime = DateTime.Now.ToString();
+                            fie.SelectSingleNode("IsFTPWT").InnerText = "1";
+                            fie.SelectSingleNode("FtpWTStartTime").InnerText = startWTime;
+                            fie.SelectSingleNode("FtpWTEndTime").InnerText = endWTime;
+                            xd.Save("E:\\WatcherFiles.xml");
+                        }
+                        else
+                        {
+                            WriteLog(fullPath + "没有成功上传.时间在" + startWTime);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// 异常信息写入日志
+        /// </summary>
+        private void WriteLog(string errorMessage)
+        {
+            string path = @"C:\WINDOWS\FileWatcherLog.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                string now = "异常发生在" + DateTime.Now.ToString() +
+                    ".详细信息为:" + errorMessage ;
+                sw.WriteLine(now);
+            }
+        }
+
+        private void myWatcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            string s = e.FullPath;
+
+            //将变动信息写入XML.
+            WriterFileXML(e.Name, e.FullPath);
+        }
+
+    }
+}
